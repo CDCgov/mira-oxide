a58633ca1a820038a01508a8d5964d6553f733b4
Samuel Shepard <vfn4@cdc.gov>
2025-05-13 11:39:02 -0400

    Adopt a format

--- mutations_of_interest_table/src/main.rs
+++ mutations_of_interest_table/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use either::Either;
 use std::{
     fs::{File, OpenOptions},
-    io::{stdin, stdout, BufRead, BufReader, BufWriter, Stdin, Write},
+    io::{BufRead, BufReader, BufWriter, Stdin, Write, stdin, stdout},
     path::PathBuf,
 };
 use zoe::alignment::sw::sw_scalar_alignment;
@@ -34,15 +34,15 @@ pub struct APDArgs {
 }
 
 pub struct Strain {
-    sample_id: String,
-    ref_strain: String,
-    gisaid_accession: String,
-    subtype: String,
-    dais_ref: String,
-    protein: String,
-    aa_ref: char,
-    position: usize,
-    aa_mut: char,
+    sample_id:               String,
+    ref_strain:              String,
+    gisaid_accession:        String,
+    subtype:                 String,
+    dais_ref:                String,
+    protein:                 String,
+    aa_ref:                  char,
+    position:                usize,
+    aa_mut:                  char,
     phenotypic_consequences: String,
 }
 
@@ -58,8 +58,9 @@ impl Strain {
             "aa_reference",
             "aa_position",
             "aa_mutation",
-            "phenotypic_consequences"
-        ].join(delim)
+            "phenotypic_consequences",
+        ]
+        .join(delim)
     }
 
     fn to_delimited(&self, delim: &str) -> String {
@@ -74,7 +75,8 @@ impl Strain {
             &self.position.to_string(),
             &self.aa_mut.to_string(),
             self.phenotypic_consequences.as_str(),
-        ].join(delim)
+        ]
+        .join(delim)
     }
 
     fn update_entry_from_alignment(&mut self, aa_1: u8, aa_2: u8, muts_columns: &[Vec<String>]) -> bool {
@@ -132,25 +134,16 @@ pub fn lines_to_vec<R: BufRead>(reader: R) -> std::io::Result<Vec<Vec<String>>> 
     Ok(columns)
 }
 
-pub fn align_sequences<'a>(
-    query: &'a [u8],
-    reference: &'a [u8],
-) -> (Vec<u8>, Vec<u8>) {
+pub fn align_sequences<'a>(query: &'a [u8], reference: &'a [u8]) -> (Vec<u8>, Vec<u8>) {
     const MAPPING: ByteIndexMap<22> = ByteIndexMap::new(*b"ACDEFGHIKLMNPQRSTVWXY*", b'X');
     const WEIGHTS: WeightMatrix<i8, 22> = WeightMatrix::new(&MAPPING, 1, 0, Some(b'X'));
     const GAP_OPEN: i8 = -1;
     const GAP_EXTEND: i8 = 0;
 
-    let profile = ScalarProfile::<22>::new(query, WEIGHTS, GAP_OPEN, GAP_EXTEND)
-        .expect("Ya beefed it");
+    let profile = ScalarProfile::<22>::new(query, WEIGHTS, GAP_OPEN, GAP_EXTEND).expect("Ya beefed it");
     let alignment = sw_scalar_alignment(reference, &profile);
 
-    pairwise_align_with_cigar(
-        reference,
-        query,
-        &alignment.cigar,
-        alignment.ref_range.start,
-    )
+    pairwise_align_with_cigar(reference, query, &alignment.cigar, alignment.ref_range.start)
 }
 
 fn main() -> std::io::Result<()> {
@@ -177,11 +170,7 @@ fn main() -> std::io::Result<()> {
     let muts_columns: Vec<Vec<String>> = lines_to_vec(muts_reader)?;
 
     let mut writer = if let Some(ref file_path) = args.output_xsv {
-        let file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(file_path)?;
+        let file = OpenOptions::new().write(true).create(true).truncate(true).open(file_path)?;
         BufWriter::new(Either::Left(file))
     } else {
         BufWriter::new(Either::Right(stdout()))
@@ -193,9 +182,7 @@ fn main() -> std::io::Result<()> {
     //Finding reference sequences in the same coordinate space to compare with
     for i in 0..columns[1].len() {
         for j in 0..ref_columns[1].len() {
-            if columns[1][i] == ref_columns[5][j]
-                && columns[2][i] == ref_columns[6][j]
-                && columns[3][i] == ref_columns[7][j]
+            if columns[1][i] == ref_columns[5][j] && columns[2][i] == ref_columns[6][j] && columns[3][i] == ref_columns[7][j]
             {
                 let aa_seq1 = ref_columns[8][j].as_bytes();
                 let aa_seq2 = columns[6][i].as_bytes();
@@ -203,15 +190,15 @@ fn main() -> std::io::Result<()> {
                 //aa seqs that are the same length will be aligned already and saves time to not align
                 if aa_seq1.len() == aa_seq2.len() {
                     let mut entry = Strain {
-                        sample_id: columns[0][i].to_string(),
-                        ref_strain: ref_columns[1][j].to_string(),
-                        gisaid_accession: ref_columns[0][j].to_string(),
-                        subtype: columns[1][i].to_string(),
-                        dais_ref: columns[2][i].to_string(),
-                        protein: columns[3][i].to_string(),
-                        position: 0,
-                        aa_ref: 'X',
-                        aa_mut: 'X',
+                        sample_id:               columns[0][i].to_string(),
+                        ref_strain:              ref_columns[1][j].to_string(),
+                        gisaid_accession:        ref_columns[0][j].to_string(),
+                        subtype:                 columns[1][i].to_string(),
+                        dais_ref:                columns[2][i].to_string(),
+                        protein:                 columns[3][i].to_string(),
+                        position:                0,
+                        aa_ref:                  'X',
+                        aa_mut:                  'X',
                         phenotypic_consequences: String::new(),
                     };
                     for aa in 0..aa_seq1.len() {
@@ -232,18 +219,18 @@ fn main() -> std::io::Result<()> {
                     //Using Zoe for alignment
                     let query = columns[6][i].as_bytes();
                     let reference = ref_columns[8][j].as_bytes();
-                    let(aligned_1, aligned_2) = align_sequences(query, reference);
+                    let (aligned_1, aligned_2) = align_sequences(query, reference);
 
                     let mut entry = Strain {
-                        sample_id: columns[0][i].to_string(),
-                        ref_strain: ref_columns[1][j].to_string(),
-                        gisaid_accession: ref_columns[0][j].to_string(),
-                        subtype: columns[1][i].to_string(),
-                        dais_ref: columns[2][i].to_string(),
-                        protein: columns[3][i].to_string(),
-                        position: 0,
-                        aa_ref: 'X',
-                        aa_mut: 'X',
+                        sample_id:               columns[0][i].to_string(),
+                        ref_strain:              ref_columns[1][j].to_string(),
+                        gisaid_accession:        ref_columns[0][j].to_string(),
+                        subtype:                 columns[1][i].to_string(),
+                        dais_ref:                columns[2][i].to_string(),
+                        protein:                 columns[3][i].to_string(),
+                        position:                0,
+                        aa_ref:                  'X',
+                        aa_mut:                  'X',
                         phenotypic_consequences: String::new(),
                     };
 
