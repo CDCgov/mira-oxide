ec9c65199a790807388390d407e2d7c186629cb0
Samuel Shepard <vfn4@cdc.gov>
2025-05-13 13:16:29 -0400

    Exploratory edits, please don't use in prod

--- mutations_of_interest_table/src/main.rs
+++ mutations_of_interest_table/src/main.rs
@@ -1,3 +1,11 @@
+#![feature(let_chains)]
+
+// TODO: replace extra work with csv + Serde and define structs to deserialization
+// Create structs
+// Derserialize to them
+// Add helper methods as need directly to the struct
+// Consider unifying errors in some less manual way
+
 use clap::Parser;
 use either::Either;
 use std::{
@@ -104,6 +112,21 @@ impl Strain {
     }
 }
 
+fn update_entry_from_alignment<'a>(
+    query_aa: char, query_pos: usize, protein: &str, mutations: &'a [MutationOfInterest],
+) -> Option<&'a str> {
+    for mutation in mutations {
+        if protein == mutation.protein && query_pos == mutation.position {
+            if query_aa == mutation.wildtype_aa {
+                return Some(mutation.description.as_str());
+            } else if query_aa == '-' {
+                return Some("amino acid information missing");
+            }
+        }
+    }
+    None
+}
+
 fn create_reader(path: Option<PathBuf>) -> std::io::Result<BufReader<Either<File, Stdin>>> {
     let reader = if let Some(ref file_path) = path {
         let file = OpenOptions::new().read(true).open(file_path)?;
@@ -115,11 +138,11 @@ fn create_reader(path: Option<PathBuf>) -> std::io::Result<BufReader<Either<File
     Ok(reader)
 }
 
-pub fn lines_to_vec<R: BufRead>(reader: R) -> std::io::Result<Vec<Vec<String>>> {
+pub fn lines_to_vec<R: BufRead>(reader: R) -> Result<Vec<Vec<String>>, ParsingErrors> {
     let mut columns: Vec<Vec<String>> = Vec::new();
 
     for line_result in reader.lines() {
-        let line = line_result?;
+        let line = line_result.map_err(|_| ParsingErrors::MissingLine)?;
         let values: Vec<_> = line.split('\t').map(str::to_owned).collect();
 
         if columns.is_empty() {
@@ -146,7 +169,67 @@ pub fn align_sequences<'a>(query: &'a [u8], reference: &'a [u8]) -> (Vec<u8>, Ve
     pairwise_align_with_cigar(reference, query, &alignment.cigar, alignment.ref_range.start)
 }
 
-fn main() -> std::io::Result<()> {
+#[derive(Debug)]
+enum ParsingErrors {
+    BadPosition,
+    MissingField,
+    MissingLine,
+}
+
+impl std::fmt::Display for ParsingErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParsingErrors::BadPosition => write!(f, "Failed to parse position as a number"),
+            ParsingErrors::MissingField => write!(f, "Missing required field in input"),
+            ParsingErrors::MissingLine => write!(f, "Missing line in file"),
+        }
+    }
+}
+
+impl std::error::Error for ParsingErrors {}
+
+// Define a Result type alias for parsing operations
+type ParseIntoResult<T> = Result<T, ParsingErrors>;
+
+impl From<std::num::ParseIntError> for ParsingErrors {
+    fn from(_: std::num::ParseIntError) -> Self {
+        ParsingErrors::BadPosition
+    }
+}
+
+struct MutationOfInterest {
+    protein:     String,
+    position:    usize,
+    wildtype_aa: char,
+    description: String,
+}
+
+impl TryFrom<&str> for MutationOfInterest {
+    type Error = ParsingErrors;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut split = value.split('\t');
+
+        let protein = split.next().ok_or(ParsingErrors::MissingField)?.to_string();
+        let position = split.next().ok_or(ParsingErrors::MissingField)?.parse::<usize>()?;
+        let wildtype_aa = split
+            .next()
+            .ok_or(ParsingErrors::MissingField)?
+            .chars()
+            .next()
+            .ok_or(ParsingErrors::MissingField)?;
+        let description = split.next().ok_or(ParsingErrors::MissingField)?.to_string();
+
+        Ok(MutationOfInterest {
+            protein,
+            position,
+            wildtype_aa,
+            description,
+        })
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = APDArgs::parse();
     // let delim = args.output_delimiter.clone();
 
@@ -164,10 +247,15 @@ fn main() -> std::io::Result<()> {
     let ref_columns: Vec<Vec<String>> = lines_to_vec(ref_reader)?;
 
     //read in mutations file (mutations of interest)
-    let muts_reader = create_reader(args.muts_file)?;
+    let mut muts_reader = create_reader(args.muts_file)?;
 
+    let mut mutations = Vec::new();
     // Initialize vectors to store columns (mutations of interest)
-    let muts_columns: Vec<Vec<String>> = lines_to_vec(muts_reader)?;
+    for line in muts_reader.lines() {
+        let line = line?;
+        let record: MutationOfInterest = line.as_str().try_into()?;
+        mutations.push(record);
+    }
 
     let mut writer = if let Some(ref file_path) = args.output_xsv {
         let file = OpenOptions::new().write(true).create(true).truncate(true).open(file_path)?;
@@ -179,6 +267,9 @@ fn main() -> std::io::Result<()> {
     //header of output file
     writeln!(&mut writer, "{}", Strain::header(&args.output_delimiter))?;
 
+    // TODO: fix me
+    let dlm = '\t';
+
     //Finding reference sequences in the same coordinate space to compare with
     for i in 0..columns[1].len() {
         for j in 0..ref_columns[1].len() {
@@ -189,29 +280,29 @@ fn main() -> std::io::Result<()> {
                 //If aa seq are the same length start seqe comparison
                 //aa seqs that are the same length will be aligned already and saves time to not align
                 if aa_seq1.len() == aa_seq2.len() {
-                    let mut entry = Strain {
-                        sample_id:               columns[0][i].to_string(),
-                        ref_strain:              ref_columns[1][j].to_string(),
-                        gisaid_accession:        ref_columns[0][j].to_string(),
-                        subtype:                 columns[1][i].to_string(),
-                        dais_ref:                columns[2][i].to_string(),
-                        protein:                 columns[3][i].to_string(),
-                        position:                0,
-                        aa_ref:                  'X',
-                        aa_mut:                  'X',
-                        phenotypic_consequences: String::new(),
-                    };
-                    for aa in 0..aa_seq1.len() {
-                        entry.position += 1;
+                    let (sample_id, ref_strain, gisaid_accession, subtype, dais_ref, protein) = (
+                        columns[0][i].as_str(),
+                        ref_columns[1][j].as_str(),
+                        ref_columns[0][j].as_str(),
+                        columns[1][i].as_str(),
+                        columns[2][i].as_str(),
+                        columns[3][i].as_str(),
+                    );
 
-                        if aa_seq1[aa] == aa_seq2[aa] {
-                        } else {
-                            //aa difference moved foraward in process
-                            entry.aa_mut = aa_seq2[aa] as char;
-                            entry.aa_ref = aa_seq1[aa] as char;
-                            if entry.update_entry_from_alignment(aa_seq1[aa], aa_seq2[aa], &muts_columns) {
-                                writeln!(&mut writer, "{}", entry.to_delimited(&args.output_delimiter))?;
-                            }
+                    for (pos, ref_aa, query_aa) in aa_seq1
+                        .iter()
+                        .zip(aa_seq2)
+                        .enumerate()
+                        .map(|(i, (&r, &q))| (i + 1, r as char, q as char))
+                    {
+                        if ref_aa != query_aa
+                            && let Some(description) = update_entry_from_alignment(query_aa, pos, protein, &mutations)
+                        {
+                            writeln!(
+                                &mut writer,
+                                "{sample_id}{dlm}{ref_strain}{dlm}{gisaid_accession}{dlm}{subtype}{dlm}\
+                                 {dais_ref}{dlm}{protein}{dlm}{pos}{dlm}{ref_aa}{dlm}{query_aa}{dlm}{description}"
+                            )?;
                         }
                     }
                 } else {
@@ -242,9 +333,11 @@ fn main() -> std::io::Result<()> {
                             //aa difference moved foraward in process
                             entry.aa_mut = aligned_2[aa] as char;
                             entry.aa_ref = aligned_1[aa] as char;
-                            if entry.update_entry_from_alignment(aligned_1[aa], aligned_2[aa], &muts_columns) {
+
+                            todo!()
+                            /*if entry.update_entry_from_alignment(aligned_1[aa], aligned_2[aa], &mutations) {
                                 writeln!(&mut writer, "{}", entry.to_delimited(&args.output_delimiter))?;
-                            }
+                            }*/
                         }
                     }
                 }
