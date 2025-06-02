eb475ea520e548dea481456eb6b79d10b9e085d0
mandysulli <xpa3@cdc.gov>
2025-06-02 13:58:59 -0400

    setting up args

--- flu_prepare_irma_json/src/main.rs
+++ flu_prepare_irma_json/src/main.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+use std::{
+    error::Error,
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, BufWriter, Stdin, Write, stdin, stdout},
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug, Parser)]
+#[command(about = "Tool for calculating amino acid difference tables")]
+pub struct APDArgs {
+    #[arg(short = 'i', long)]
+    /// Optional input fasta
+    irma_path: Option<PathBuf>,
+
+    #[arg(short = 's', long)]
+    /// Optional input fasta
+    samplesheet: Option<PathBuf>,
+
+    #[arg(short = 'p', long)]
+    /// Optional input fasta
+    platform: Option<String>,
+
+    #[arg(short = 'w', long)]
+    /// Optional output delimited file
+    workdir_path: Option<PathBuf>,
+
+    #[arg(short = 'c', long, default_value = ",")]
+    /// Use the provider delimiter for separating fields. Default is ','
+    irma_config: Option<PathBuf>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
