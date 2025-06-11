53429d7f2473639f6d413ef4d719b99c58d50636
Sam Wiley <dzw2@cdp-client-02.biotech.cdc.gov>
2025-06-11 16:27:05 -0400

    wip for check chemistry

--- check_chemistry/src/main.rs
+++ check_chemistry/src/main.rs
@@ -1,8 +1,10 @@
-use std::path::PathBuf;
+use std::{fmt, fs::File, io::BufReader, path::PathBuf};
+use zoe::prelude::*;
+use clap::{builder::PossibleValue, Parser, ValueEnum};
 
-use clap::{Args, ValueEnum, builder::PossibleValue};
 
-#[derive(Args, Debug)]
+#[derive(Debug, Parser)]
+#[command(about = "Tool for calculating amino acid difference tables")]
 pub struct CheckChemArgs {
     #[arg(short = 's', long)]
     /// Name of sample
@@ -94,8 +96,62 @@ impl ValueEnum for IRMAConfig {
     }
 }
 
+#[derive(Debug)]
+pub enum IrmaModule {
+    FLU,
+    CoV,
+    RSV,
+    FLUMinion,
+    CoVsGene,
+}
+
+impl fmt::Display for IrmaModule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            IrmaModule::FLU => write!(f, "FLU"),
+            IrmaModule::CoV => write!(f, "CoV"),
+            IrmaModule::RSV => write!(f, "RSV"),
+            IrmaModule::FLUMinion => write!(f, "FLU-minion"),
+            IrmaModule::CoVsGene => write!(f, "CoV-s-gene"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ChemistryOutput {
+    pub sample: String,
+    pub irma_custom: String,
+    pub subsample: usize,
+    pub irma_module: IrmaModule,
+}
+
+fn get_average_line_length(fastq: &PathBuf) -> Result<usize, std::io::Error> {
+    let sample_size = 5;
+    let file = File::open(&fastq)?;
+    let buf_reader = BufReader::new(file);
+    let fastq_reader = FastQReader::new(buf_reader);
+    let avg_length = fastq_reader.take(sample_size).map(|fastq| {
+        let fastq = fastq.expect("Could not read fastq!");
+        fastq.sequence.len()
+    }).sum::<usize>() as f64 / sample_size as f64;
+    Ok(avg_length as usize)
+}
+
+fn parse_chemistry_args(args: &CheckChemArgs) -> Result<ChemistryOutput, std::io::Error> {
+    let _line_length = get_average_line_length(&args.fastq);
+    let out = ChemistryOutput {
+        sample: "".to_owned(),
+        irma_custom: "".to_owned(),
+        subsample: 0usize,
+        irma_module: IrmaModule::CoV,
+    };
+    Ok(out)
+}
+
 fn main() {
-    todo!()
+    let args= CheckChemArgs::parse();
+    let args = parse_chemistry_args(&args).unwrap();
+    println!("{:?}", args);
     // outputs: headers for a "chemistry csv"
-    // sample, irma_custom_0, irma_custom_1, subsample_ IRMA_module
+    // sample, irma_custom_0, irma_custom_1, subsample, IRMA_module
 }
