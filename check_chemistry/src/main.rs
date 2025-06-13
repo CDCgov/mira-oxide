7b0327208231ec0c5d4b608633bddabfdce55035
Sam Wiley <dzw2@cdp-client-02.biotech.cdc.gov>
2025-06-13 11:30:05 -0400

    working illumina chemistry

--- check_chemistry/src/main.rs
+++ check_chemistry/src/main.rs
@@ -1,7 +1,11 @@
-use std::{fmt, fs::File, io::BufReader, path::PathBuf};
+use clap::{Parser, ValueEnum, builder::PossibleValue};
+use std::{
+    fmt,
+    fs::{File, OpenOptions},
+    io::{BufReader, BufWriter, Write},
+    path::PathBuf,
+};
 use zoe::prelude::*;
-use clap::{builder::PossibleValue, Parser, ValueEnum};
-
 
 #[derive(Debug, Parser)]
 #[command(about = "Tool for calculating amino acid difference tables")]
@@ -9,14 +13,14 @@ pub struct CheckChemArgs {
     #[arg(short = 's', long)]
     /// Name of sample
     pub sample: String,
-    
+
     #[arg(short = 'q', long)]
     /// Path to fastq file
     pub fastq: PathBuf,
 
-    #[arg(short = 'r', long)]
+    //#[arg(short = 'r', long)]
     /// Run ID
-    pub run_id: usize,
+    //pub run_id: usize,
 
     #[arg(short = 'e', long)]
     /// Experiment type
@@ -60,7 +64,9 @@ impl ValueEnum for Experiment {
     fn to_possible_value(&self) -> Option<PossibleValue> {
         match self {
             Experiment::FluIllumina => Some(PossibleValue::new("FluIllumina")),
-            Experiment::SC2WholeGenomeIllumina => Some(PossibleValue::new("SC2WholeGenomeIllumina")),
+            Experiment::SC2WholeGenomeIllumina => {
+                Some(PossibleValue::new("SC2WholeGenomeIllumina"))
+            }
             Experiment::RSVIllumina => Some(PossibleValue::new("RSVIllumina")),
         }
     }
@@ -74,54 +80,59 @@ pub enum IRMAConfig {
     Custom,
 }
 
+fn get_config_path(args: &CheckChemArgs, seq_len: Option<usize>) -> String {
+    if args.irma_config == Some(IRMAConfig::Custom) {
+        return args
+            .irma_config_path
+            .as_ref()
+            .expect("Custom IRMA config specified but no path to config file was provided")
+            .to_str()
+            .expect("Failed to convert IRMA config path to string")
+            .to_string();
+    }
 
-fn get_config_path(experiment: Experiment, seq_len: Option<usize>, irma_config: Option<IRMAConfig>) -> String {
-    let wd_path = "get this from args";
-    let path_extension = match (experiment, seq_len, irma_config) {
-        (_, None, _) => "".to_owned(),
-        (_, _, Some(irma_config)) => {
-            match irma_config {
-                IRMAConfig::Sensitive => format!("{wd_path}/bin/irma_config/FLU-sensitive.sh"),
-                IRMAConfig::Secondary => format!("{wd_path}/bin/irma_config/FLU-secondary.sh"),
-                IRMAConfig::UTR => format!("{wd_path}/bin/irma_config/FLU-utr.sh"),
-                IRMAConfig::Custom => format!("{wd_path} we gonna have to do something else here"),
-            }
-        }
+    let path_extension = match (args.experiment, seq_len, args.irma_config) {
+        (_, None, _) => return "".to_string(),
+        (_, _, Some(irma_config)) => match irma_config {
+            IRMAConfig::Sensitive => "/bin/irma_config/FLU-sensitive.sh",
+            IRMAConfig::Secondary => "/bin/irma_config/FLU-secondary.sh",
+            IRMAConfig::UTR => "/bin/irma_config/FLU-utr.sh",
+            IRMAConfig::Custom => unreachable!(),
+        },
         (Experiment::FluIllumina, Some(seq_len), None) => {
             if seq_len >= 145 {
-                format!("{wd_path}/bin/irma_config/FLU.sh")
+                "/bin/irma_config/FLU.sh"
             } else {
-                format!("{wd_path}/bin/irma_config/FLU-2x75.sh")
+                "/bin/irma_config/FLU-2x75.sh"
             }
         }
         (Experiment::SC2WholeGenomeIllumina, Some(seq_len), None) => {
             if seq_len > 80 {
-                format!("{wd_path}/bin/irma_config/CoV.sh")
+                "/bin/irma_config/CoV.sh"
             } else {
-                format!("{wd_path}/bin/irma_config/SC2-2x75.sh")
+                "/bin/irma_config/SC2-2x75.sh"
             }
         }
         (Experiment::RSVIllumina, Some(seq_len), None) => {
             if seq_len > 80 {
-                format!("{wd_path}/bin/irma_config/RSV.sh")
+                "/bin/irma_config/RSV.sh"
             } else {
-                format!("{wd_path}/bin/irma_config/RSV-2x75.sh")
+                "/bin/irma_config/RSV-2x75.sh"
             }
         }
     };
-    path_extension
-} 
 
+    let wd_path = args
+        .wd_path
+        .to_str()
+        .expect("Failed to convert work directory path to string");
+    format!("{}{}", wd_path, path_extension)
+}
 
 impl ValueEnum for IRMAConfig {
     #[inline]
     fn value_variants<'a>() -> &'a [Self] {
-        &[
-            Self::Sensitive,
-            Self::Secondary,
-            Self::UTR,
-            Self::Custom,
-        ]
+        &[Self::Sensitive, Self::Secondary, Self::UTR, Self::Custom]
     }
 
     #[inline]
@@ -164,6 +175,16 @@ pub struct ChemistryOutput {
     pub irma_module: IrmaModule,
 }
 
+impl fmt::Display for ChemistryOutput {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{},{},{},{}",
+            self.sample, self.irma_custom, self.subsample, self.irma_module
+        )
+    }
+}
+
 /// Averages the first five sequence lengths if possible. If the file has no
 /// sequences, returns None
 fn get_average_line_length(fastq: &PathBuf) -> Result<Option<usize>, std::io::Error> {
@@ -171,7 +192,7 @@ fn get_average_line_length(fastq: &PathBuf) -> Result<Option<usize>, std::io::Er
     let file = File::open(&fastq)?;
     let buf_reader = BufReader::new(file);
     let fastq_reader = FastQReader::new(buf_reader);
-    
+
     let mut total_len = 0;
     let mut count = 0;
 
@@ -190,9 +211,8 @@ fn get_average_line_length(fastq: &PathBuf) -> Result<Option<usize>, std::io::Er
 
 fn parse_chemistry_args(args: &CheckChemArgs) -> Result<ChemistryOutput, std::io::Error> {
     let line_length = get_average_line_length(&args.fastq)?;
-    
-    
-    let irma_custom = get_config_path(args.experiment, line_length, args.irma_config);
+
+    let irma_custom = get_config_path(args, line_length);
     let out = ChemistryOutput {
         sample: args.sample.clone(),
         irma_custom,
@@ -202,10 +222,24 @@ fn parse_chemistry_args(args: &CheckChemArgs) -> Result<ChemistryOutput, std::io
     Ok(out)
 }
 
-fn main() {
-    let args= CheckChemArgs::parse();
-    let args = parse_chemistry_args(&args).unwrap();
-    println!("{:?}", args);
+fn main() -> Result<(), std::io::Error> {
+    let args = CheckChemArgs::parse();
+    let output = parse_chemistry_args(&args)?;
+    let filename = format!("{}_chemistry.csv", args.sample);
+    let headers = "sample_ID,irma_custom,subsample,irma_module";
+
+    let mut writer = {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(filename)?;
+        BufWriter::new(file)
+    };
+    writeln!(&mut writer, "{headers}")?;
+    writeln!(&mut writer, "{output}")?;
+    writer.flush()?;
     // outputs: headers for a "chemistry csv"
     // sample, irma_custom_0, irma_custom_1, subsample, IRMA_module
+    Ok(())
 }
