7cb96da34a3c769aa698e1ec25e6b1164c6b8434
mandysulli <xpa3@cdc.gov>
2025-07-21 10:50:24 -0400

    add code for creating cov df

--- src/processes/prepare_mira_reports.rs
+++ src/processes/prepare_mira_reports.rs
@@ -2,6 +2,7 @@
 use clap::Parser;
 use csv::ReaderBuilder;
 use either::Either;
+use glob::glob;
 use polars::prelude::*;
 use serde::{self, Deserialize, de::DeserializeOwned};
 use std::{
@@ -108,6 +109,45 @@ fn read_yaml<R: std::io::Read>(reader: R) -> Result<QCConfig, Box<dyn std::error
     Ok(config)
 }
 
+fn coverage_df(irma_path: &PathBuf) -> Result<DataFrame, Box<dyn Error>> {
+    // Define the pattern to match text files
+    let pattern = format!(
+        "{}/*/IRMA/*/tables/*coverage.txt",
+        irma_path.to_string_lossy()
+    );
+    println!("{}", pattern);
+
+    // Initialize an empty DataFrame to hold the combined data
+    let mut combined_cov_df: Option<DataFrame> = None;
+
+    // Iterate over all files matching the pattern
+    for entry in glob(&pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                // Read the CSV file into a DataFrame
+                let file_path = path.to_str().unwrap();
+                println!("Reading file: {}", file_path);
+
+                let df = CsvReader::from_path(file_path)?.has_header(true).finish()?;
+
+                // Combine the DataFrame with the existing one
+                combined_cov_df = match combined_cov_df {
+                    Some(existing_df) => Some(existing_df.vstack(&df)?),
+                    None => Some(df),
+                };
+            }
+            Err(e) => println!("Error reading file: {}", e),
+        }
+    }
+
+    // Return the combined DataFrame or an error if no data was found
+    if let Some(df) = combined_cov_df {
+        Ok(df)
+    } else {
+        Err("No files found or no data to combine.".into())
+    }
+}
+
 pub fn prepare_mira_reports_process(args: ReportsArgs) -> Result<(), Box<dyn Error>> {
     // Read in samplesheet
     let samplesheet = read_csv_to_dataframe(&args.samplesheet)?;
@@ -116,9 +156,13 @@ pub fn prepare_mira_reports_process(args: ReportsArgs) -> Result<(), Box<dyn Err
     let qc_yaml_path = create_reader(Some(args.qc_yaml))?;
     let qc_config: QCConfig = read_yaml(qc_yaml_path)?;
 
+    //cov df
+    let cov_df = coverage_df(&args.irma_path)?;
+
     //println!("{:?}", samplesheet);
     println!("{:?}", qc_config);
     println!("{:?}", samplesheet);
+    println!("{:?}", cov_df);
 
     Ok(())
 }
